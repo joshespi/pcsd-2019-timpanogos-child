c34dc3d6a86ead9708d53c9c1fbd0e9eb1decbfa
Josh Espinoza <joshespi@gmial.com>
2019-06-26 15:35:37 -0600

    06-26-19 updates

--- timpanogos-child/sidebar.php
+++ timpanogos-child/sidebar.php
@@ -43,7 +43,7 @@
 						</a>
 					</li>
 					<li>
-						<a href="<?php echo get_home_url(); ?>/all-events-calendar/">
+						<a href="<?php echo get_home_url(); ?>/school-information/all-events-calendar/">
 							<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/all-events-calendar.svg" alt="" />
 							<span>All Events Calendar</span>
 						</a>
@@ -67,6 +67,7 @@
 						</a>
 					</li>
 				</ul>
+				<a href="https://www.peachjar.com/index.php?region=33080&a=28&b=138"><img src="https://timpanogos.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
 				<a href="https://safeut.med.utah.edu/"><img src="http://provohigh.provo.edu/wp-content/uploads/2017/02/safeUTcrisisline.jpg" alt="SafeUT Link" /></a>
 				
 			</section>
